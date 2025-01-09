8dd19d278a747f1f6a7de8efe03f89257f4d9b70
HerisonMaciel <herisson2maciel@hotmail.com>
2025-01-08 22:57:37 -0300

    corretion build

--- src/main/java/com/herison/cep/infrastructure/ZipCodeMongoConfig.java
+++ src/main/java/com/herison/cep/infrastructure/ZipCodeMongoConfig.java
@@ -2,14 +2,11 @@ package com.herison.cep.infrastructure;
 
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
-import org.springframework.data.mongodb.MongoDatabaseFactory;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
 import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.data.mongodb.core.SimpleMongoClientDatabaseFactory;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @Configuration
